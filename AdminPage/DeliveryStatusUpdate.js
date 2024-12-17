3795c043da0a779407f13b1c969b50cb7db9ef6d
KwakKwikKwok <cutekenzie2017@gmail.com>
2024-12-17 15:38:31 +0700

    Final Version - Rev 4.1 [FilterDropdown.js]

--- AdminPage/DeliveryStatusUpdate.js
+++ AdminPage/DeliveryStatusUpdate.js
@@ -1,11 +1,18 @@
+function updateButtonText(dropdownId, selectedValue) {
+    const dropdownButton = document.getElementById(dropdownId);
+    if (dropdownButton) {
+        dropdownButton.textContent = selectedValue;
+    }
+}
+
 function updateButtonClass(buttonId, status) {
     var button = document.getElementById(buttonId);
 
     button.innerText = status;
     if (status === 'Delivered') {
         button.classList.remove('btn-danger');
-        button.classList.add('btn-info'); // Green for delivered
+        button.classList.add('btn-info');
     } else {
-        button.classList.add('btn-danger'); // Red for not delivered
+        button.classList.add('btn-danger');
     }
 }
--- AdminPage/PaymentApproval.js
+++ AdminPage/PaymentApproval.js
@@ -1,3 +1,8 @@
+function updateButtonText(dropdownId, text) {
+    var button = document.getElementById(dropdownId);
+    button.textContent = text;
+}
+
 function verifyPayment(button) {
     let isConfirmed = confirm("Are you sure you want to approve this payment?");
     if (isConfirmed) {
